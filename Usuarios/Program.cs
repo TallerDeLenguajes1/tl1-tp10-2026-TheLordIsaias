cc85e3396e6223b271b68818cc47d6baea0bb84d
Miguel Isaias Ruiz Selman <miruizselman@gmail.com>
2026-06-16 16:48:51 -0300

    Empiezo a trabajar Punto 2

---
+++ Usuarios/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using Usuarios;
+
+HttpClient clienteHttp = new ();
+
+const string urlApi = "https://jsonplaceholder.typicode.com/users";
+const string archivoSalida = "usuarios.json";
+
+
+HttpResponseMessage respuesta = await clienteHttp.GetAsync(urlApi);
+respuesta.EnsureSuccessStatusCode();
+
+string jsonCrudo = await respuesta.Content.ReadAsStringAsync();
+
+List<Usuario>? listaUsuarios = new ();
+
+listaUsuarios = JsonSerializer.Deserialize<List<Usuario>>(jsonCrudo);
+
+if(listaUsuarios != null)
+{
+    List<Usuario> primeros = listaUsuarios.Take(5).ToList();
+    foreach (var usuario in primeros)
+    {
+        Console.WriteLine($"Nombre: {usuario.name}\nCorreo Electronico: {usuario.email}\nDomicilio: {usuario.address.street}");
+    }
+    string jsonString = JsonSerializer.Serialize(listaUsuarios);
+    
+    File.WriteAllText(archivoSalida, jsonString);
+} else
+{
+    Console.WriteLine("No se pudo recuperar la lista de Tareas");
+}
---
+++ Usuarios/Usuarios.cs
@@ -0,0 +1,21 @@
+namespace Usuarios{
+    public class Usuario{
+        public int id{get; set;}
+        public string name{get; set;}
+        public string username{get; set;}
+        public string email{get; set;}
+        public Direccion address{get; set;}
+    }
+    public class Direccion{
+        public string street{get; set;}
+        public string suite{get; set;}
+        public string city{get; set;}
+        public string zipcode{get; set;}
+        public Geolocalizacion geo{get; set;}
+    }
+    public class Geolocalizacion
+        {
+            public string lat{get; set;}
+            public string lng{get; set;}
+        }
+}
