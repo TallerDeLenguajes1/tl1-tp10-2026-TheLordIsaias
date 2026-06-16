ad3220591fc4e97b92aa79495cd629baa3b89fd1
Miguel Isaias Ruiz Selman <miruizselman@gmail.com>
2026-06-16 16:12:41 -0300

    asd

---
+++ Tareas/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json;
+using Tareas;
+
+HttpClient clienteHttp = new ();
+
+const string urlApi = "https://jsonplaceholder.typicode.com/todos";
+const string archivoSalida = "tareas.json";
+
+
+HttpResponseMessage respuesta = await clienteHttp.GetAsync(urlApi);
+respuesta.EnsureSuccessStatusCode();
+
+string jsonCrudo = await respuesta.Content.ReadAsStringAsync();
+
+List<Tarea>? listaTareas = new ();
+
+listaTareas = JsonSerializer.Deserialize<List<Tarea>>(jsonCrudo);
+
+if(listaTareas != null)
+{
+    Console.WriteLine("----- Tareas No Completadas -----");
+    foreach (var tarea in listaTareas)
+    {
+        if (!tarea.Completado)
+        {
+          Console.WriteLine($"ID Usuario: {tarea.UserID}\n-ID Tarea {tarea.IdTarea}\n-Titulo de la Tarea: {tarea.Titulo}\nCompletado: {tarea.Completado}");  
+        }
+        
+    }
+    Console.WriteLine("----- Tareas Completadas -----");
+    foreach (var tarea in listaTareas)
+    {
+        if (tarea.Completado)
+        {
+          Console.WriteLine($"ID Usuario: {tarea.UserID}\n-ID Tarea {tarea.IdTarea}\n-Titulo de la Tarea: {tarea.Titulo}\nCompletado: {tarea.Completado}");  
+        }
+        
+    }
+    string jsonString = JsonSerializer.Serialize(listaTareas);
+    
+    File.WriteAllText(archivoSalida, jsonString);
+} else
+{
+    Console.WriteLine("No se pudo recuperar la lista de Tareas");
+}
+
---
+++ Tareas/Tarea.cs
@@ -0,0 +1,11 @@
+namespace Tareas
+{
+    public class Tarea
+    {
+        public int UserID{get; set;}
+        public int IdTarea{get; set;}
+        public string Titulo{get; set;}
+        public bool Completado{get; set;}
+
+    }
+}
